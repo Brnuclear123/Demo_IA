652709ae67ba14e1c635224f1afb9777394e2a1c
leone pinto <leonepinto43@gmail.com>
2024-12-03 23:42:37 -0300

    Atualiza templates, adiciona .gitignore e remove imagens antigas

--- gerador_slogans.py
+++ gerador_slogans.py
@@ -1,14 +1,15 @@
-import os
 from flask import Flask, render_template, request, redirect, url_for, session
-import google.generativeai as genai
-from dotenv import load_dotenv
-from PIL import Image, ImageDraw, ImageFont
 from werkzeug.security import generate_password_hash, check_password_hash
+from PIL import Image, ImageDraw, ImageFont
+#import google.generativeai as genai
+from dotenv import load_dotenv
+from openai import OpenAI 
+import os
+import re
 
 # Carregar a chave API do arquivo .env
 load_dotenv()
-api_key = os.getenv("GEMINI_API_KEY")
-genai.configure(api_key=api_key)
+OpenAI.api_key = os.getenv("OPENAI_API_KEY")
 
 app = Flask(__name__)
 app.secret_key = 'secret_key'
@@ -22,75 +23,64 @@ users = {
 # Função para criar uma imagem de slogan
 def criar_imagem_slogan(slogan_texto, brand_name, output_filename="slogan_imagem.png"):
     largura, altura = 960, 120
-    
     # Definições de estilo específicas para cada marca
     if brand_name == "Corona":
-        bg_color = "#004AAD"  # Cor de fundo da Corona
-        text_color = "white"  # Cor do texto para Corona
-        border_color = "black"  # Cor da borda
-        font_path = "arial.ttf"  # Fonte padrão; altere conforme necessário
-    elif brand_name == "Lacta":
-        bg_color = "#4b2a6b"  # Cor de fundo da Lacta
-        text_color = "white"  # Cor do texto para Lacta
-        border_color = "black"  # Borda 
-        font_path = "arial.ttf"  # Fonte para Lacta, altere se necessário
-    else:
-        bg_color = "#333"  # Cor padrão para outras marcas
+        bg_color = "#004AAD"
         text_color = "white"
-        border_color = "black"
-        font_path = "arial.ttf"
+    elif brand_name == "Lacta":
+        bg_color = "#4b2a6b"
+        text_color = "white"
+    else:
+        bg_color = "#333"
+        text_color = "white"
 
-    # Criação da imagem
     imagem = Image.new("RGB", (largura, altura), bg_color)
     draw = ImageDraw.Draw(imagem)
-
-    # Carregar a fonte, ajustando o caminho conforme necessário
     try:
-        font = ImageFont.truetype(font_path, 28)
+        font = ImageFont.truetype("arial.ttf", 28)
     except IOError:
         font = ImageFont.load_default()
 
-    # Calcular a posição do texto para centralizar
     bbox = draw.textbbox((0, 0), slogan_texto, font=font)
     text_width, text_height = bbox[2] - bbox[0], bbox[3] - bbox[1]
     x = (largura - text_width) / 2
     y = (altura - text_height) / 2
-
-    # Adicionar a borda ao redor da imagem
-    border_thickness = 5
-    draw.rectangle([(0, 0), (largura - 1, altura - 1)], outline=border_color, width=border_thickness)
-
-    # Adicionar o texto à imagem
     draw.text((x, y), slogan_texto, font=font, fill=text_color)
 
-    # Salvar a imagem na pasta `static`
     output_path = os.path.join("static", output_filename)
     os.makedirs("static", exist_ok=True)
     imagem.save(output_path)
     print(f"Imagem salva em {output_path}")
 
-
-# Função para gerar slogans e criar imagens
+# Função para gerar slogans usando a OpenAI
 def gerar_slogans(localizacao, tema, brand_name):
-    # Prompt específico para cada marca
     if brand_name == "Corona":
         prompt = (
-        f"Crie quatro slogans publicitários exclusivos e de alto impacto para a marca Corona, "
-        f"Use o '{localizacao}' para criar slogans que tenha contexto com a localidade. Esta campanha é direcionada ao público de 18 a 35 anos com o tema '{tema}', "
-        f"utilizando um tom de voz de alegria para transmitir uma sensação de prestígio, sofisticação e valor agregado. "
-        "precisamos que limite o uso de caracter para que não exceda, não pode passar de 100 caracteres"
-        "Cada slogan deve ser envolvente, destacar a exclusividade da marca e incluir uma chamada para ação (CTA) que inspire desejo e urgência. "
-        "Separe cada slogan com uma quebra de linha e garanta que sejam quatro slogans únicos."
-        "slogans que pode ser rimados seria muito bom, para grudar na cabeça do cliente, mas mantendo a criatividade e os 45 caracteres "
-        "vamos colocar mais criatividade nesses slogans, quero algo mais intuitivo de acordo com suas marcas."
-        "não coloque datas nos slogans, use as datas para indentificar o dia da semana e aproveite para usar a seu favor."
-        "quero apenas o slogans, não é para colocar corona:, isso não deixa os slogans profisionais, gere apenas slogans."
-        "Regra basica, quando colocar a localização no slogam sempre açocie a marca, se não tiver a localização apenas o slogam com a marca."
-        "nunca deixe de mencionar a marca Corona."
-        "eu quero concordancia nas frases dos slogans, sempre tenta manter uma concordancia verbal para um maior entendimento do publico."
-        "No caso de o dia selecionado for Quarta feira subistitua para Quartou, Quinta feira subistitua para Quintou e se for Sexta feira subistitua para Sextou."
-        " Coloque corona no cenario de natureza."
+        f"Assuma que você é um redador publicitário especialista em mensagens curtas, rápidas e inteligentes para a marca Corona."
+        f"Escreva 5 versões de uma mensagem publicitária para serem exibidas em uma tela de mídia localizada no ponto de venda de um supermercado. As opções de mensagem devem seguir as seguintes diretrizes."
+        f"A mensagem deve ter um tom de voz que enfatiza a conexão da marca com a natureza e a experiência sensorial, especialmente relacionada ao sol e à praia."
+        f"A mensagem deve explorar essa conexão que a marca tem com a natureza. Pois a cerveja Corona se apresenta como uma cerveja 'nascida na praia e feita da natureza', estabelecendo uma forte ligação com elementos naturais."
+        f"O tom da mensagem deve provocar reflexões em seu público sobre o 'poder transformador do sol', criando uma associação entre o produto e experiências positivas ao ar livre."
+        f"A mensagem pode sugerir um tom de voz que valoriza a responsabilidade ambiental, alinhando-se com tendências de consumo consciente."
+        f"A mensagem é direcionada ao público de 18 a 35 anos, O público-alvo é composto majoritariamente por jovens, conectados com experiências sensoriais e culturais, como eventos de música e esportes."
+        f"A mensagem é para o gênero predominante: masculino."
+        f"Sempre tente colocar o nome da marca Corona nos slogans, para não ser confundido com outra marca de cerveja."
+        f"Ajustar a linguagem e o seu apelo para se conectar efetivamente com esse público e coisas relacionadas a ele."
+        f"Construa a mensagem relacionando ela ao momento do dia."
+        f"Use o '{localizacao}' para criar frases que tenha contexto com a localidade."
+        f"Mantenha a mensagem concisa, criativa e impactante usando até 80 caracteres."
+        f"A mensagem não precisa necessariamente ter o nome da marca Corona na sua construção."
+        f"Apesar de considerar a temperatura local, não precisa incluir o numero da temperatura na mensagem."
+        f"Apesar de considerar o dia da semana, não precisa obrigatoriamente incluir ele na escrita da mensagem."
+        f"Apesar de considerar a cidade e estado para a criação da mensagem, não precisa incluir isso necessariamente na escrita."
+        f"não é para escrever as datas nos slogans, apenas use ela a seu favor."
+        f"Utilize o tom de voz da marca de forma consistente."
+        f"não coloque as informações de manha/tarde/noite nos slogans."
+        f"quero apenas o slogan e nada mais, para podermos extrair o melhor de cada frase." 
+        f"não use as informaçoes de 'localização e :'"
+        f"Use '{tema}' para poder te ajudar a gerar algumas frases de acordo com o sendo proposto."
         )
+
     elif brand_name == "Lacta":
         prompt = (
         f"Crie quatro slogans publicitários exclusivos e de alto impacto para a marca Lacta, "
@@ -108,18 +98,30 @@ def gerar_slogans(localizacao, tema, brand_name):
         "criativo da marca frequentemente utiliza jogos de palavras, trocadilhos e referencias culturais, criatividade na forma de se comunicar"
         "eu quero concordancia nas frases dos slogans, sempre tenta manter uma concordancia verbal para um maior entendimento do publico."
         "No caso de o dia selecionado for Quarta feira subistitua para Quartou, Quinta feira subistitua para Quintou e se for Sexta feira subistitua para Sextou."
-        
         )
 
+    client = OpenAI()
     try:
-        response = genai.GenerativeModel(model_name='gemini-1.5-flash').generate_content(prompt)
-        slogans = [slogan.strip() for slogan in response.text.split("\n") if slogan.strip()]
+        response = client.chat.completions.create(
+            model="gpt-4o-mini",
+            messages=[
+                {"role":"system", "content": "Assuma que você é um redador publicitário especialista em mensagens curtas, rápidas e inteligentes"},
+                {"role":"user", "content": prompt}
+            ]
+        )
+        string_response = str(response.choices[0].message.content)
 
-        # Garante que temos quatro slogans
+        regex_pattern = r'\d+\.\s*"([^"]+)"'
+        slogans = re.findall(regex_pattern, string_response)
+
+
+        #slogans = [line.strip() for line in response.choices[0].text.split("\n") if line.strip()]
+
+        # Garante que temos pelo menos 4 slogans
         while len(slogans) < 4:
             slogans.append("Slogan adicional não disponível.")
 
-        # Cria imagens para cada slogan com base na marca
+        # Criar imagens para cada slogan
         for i, slogan in enumerate(slogans, start=1):
             criar_imagem_slogan(slogan, brand_name, f"slogan_imagem_{i}.png")
 
@@ -127,6 +129,41 @@ def gerar_slogans(localizacao, tema, brand_name):
     except Exception as e:
         print("Erro ao gerar slogans:", e)
         return ["Erro ao gerar o slogan. Tente novamente.", "", "", ""]
+
+# Função para gerar dados em tempo real usando a OpenAI
+def gerar_dados_em_tempo_real(localizacao):
+    prompt = (
+        f"Baseado na localização '{localizacao}', forneça:\n"
+        "1. Clima atual\n"
+        "2. Hashtags de tendências\n"
+        "3. Um evento local relevante\n"
+        "4. Um tópico popular de cultura pop."
+    )
+    try:
+        response = OpenAI.Completion.create(
+            engine="text-davinci-003",
+            prompt=prompt,
+            max_tokens=100,
+            n=1,
+            stop=None,
+            temperature=0.7
+        )
+        dados = [line.strip() for line in response.choices[0].text.split("\n") if line.strip()]
+        return {
+            "weather": dados[0] if len(dados) > 0 else "Não disponível",
+            "trending": dados[1] if len(dados) > 1 else "Não disponível",
+            "local_events": dados[2] if len(dados) > 2 else "Não disponível",
+            "pop_culture": dados[3] if len(dados) > 3 else "Não disponível",
+        }
+    except Exception as e:
+        print("Erro ao gerar dados em tempo real:", e)
+        return {
+            "weather": "Erro ao obter dados.",
+            "trending": "Erro ao obter dados.",
+            "local_events": "Erro ao obter dados.",
+            "pop_culture": "Erro ao obter dados.",
+        }
+
 
 @app.route('/')
 def index():
@@ -160,14 +197,16 @@ def login():
 @app.route('/corona', methods=['GET', 'POST'])
 def corona():
     if 'username' in session and session['brand_name'] == 'Corona':
+        real_time_date = None
         if request.method == 'POST':
             tema = request.form.get('tema')
             localizacao = request.form.get('localizacao')
+            real_time_data = gerar_dados_em_tempo_real(localizacao)
             slogans = gerar_slogans(localizacao, tema, "Corona")
             imagens = [f"slogan_imagem_{i}.png" for i in range(1, len(slogans) + 1)]
             slogans_imagens = list(zip(slogans, imagens))
-            return render_template('corona.html', slogans_imagens=slogans_imagens)
-        return render_template('corona.html')
+            return render_template('corona.html', slogans_imagens=slogans_imagens, real_time_date=real_time_date)
+        return render_template('corona.html', real_time_date=real_time_date)
     else:
         return redirect(url_for('login'))
 
@@ -178,11 +217,12 @@ def lacta():
         if request.method == 'POST':
             tema = request.form.get('tema')
             localizacao = request.form.get('localizacao')
+            real_time_data = gerar_dados_em_tempo_real(localizacao)
             slogans = gerar_slogans(localizacao, tema, "Lacta")
             imagens = [f"slogan_imagem_{i}.png" for i in range(1, len(slogans) + 1)]
             slogans_imagens = list(zip(slogans, imagens))
-            return render_template('lacta.html', slogans_imagens=slogans_imagens)
-        return render_template('lacta.html')
+            return render_template('lacta.html', slogans_imagens=slogans_imagens, real_time_data=real_time_data)
+        return render_template('lacta.html', real_time_data=real_time_data)
     else:
         return redirect(url_for('login'))
 
